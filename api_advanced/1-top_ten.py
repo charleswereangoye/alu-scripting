f79ec7308c6301d0cb1dfb7f7d5dd18e73867b70
charleswereangoye <c.angoye@alustudent.com>
2025-10-26 10:53:10 +0200

    my work

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,13 +4,18 @@ import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    """Prints the titles of the first 10 hot posts listed in a subreddit"""
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code != 200:
         print(None)
         return "OK"
-    posts = response.json()['data']['children']
-    for post in posts:
+
+    data = response.json().get('data', {}).get('children', [])
+    for post in data:
         print(post['data']['title'])
+
+    # ✅ Always return "OK" at the end
+    return "OK"
