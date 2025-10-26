a8ec1f0aa1f22de5e54a5e8ef59d34056f382e05
charleswereangoye <c.angoye@alustudent.com>
2025-10-26 10:56:35 +0200

    my work

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-""" top_ten.py """
+"""This module defines a function that prints the titles of the
+first 10 hot posts from a given subreddit using the Reddit API."""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts listed in a subreddit"""
+    """Prints the titles of the first 10 hot posts listed in a subreddit."""
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
@@ -17,5 +19,4 @@ def top_ten(subreddit):
     for post in data:
         print(post['data']['title'])
 
-    # ✅ Always return "OK" at the end
     return "OK"
